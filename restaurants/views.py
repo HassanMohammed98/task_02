bb3be56b4ad1126128c5cede58761df46cee71a0
Hassan Mohammed <hassan-n98@hotmail.com>
2022-04-18 02:42:47 +0300

    finish -task1

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def function_name(request):
+    return HttpResponse("<h1> Hello</h1>")
+
+def home(request):
+    context = {
+        'msg': "Hello World!",
+    }
+    return render(request, 'home_page.html', context)
